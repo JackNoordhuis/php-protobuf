e6772bfc4ef60aaf408d3b2db6f14dd24b53ba38
Jack Noordhuis <daswaggamer@gmail.com>
2018-05-15 13:14:51 +1000

    Updates to make global composer installation work

--- protoc-gen-php.php
+++ protoc-gen-php.php
@@ -1,18 +1,15 @@
 #!/usr/bin/env php
 <?php
 
-use Allegro\Protobuf\Compiler\Logger;
-
-$composerAutoload = __DIR__ . '/vendor/autoload.php';
-if(!file_exists($composerAutoload)) {
+if(!file_exists(__DIR__ . '/vendor/autoload.php')) {
     echo '[ERROR] The dependencies are not installed. Run "composer install" in order to install them.' . PHP_EOL . PHP_EOL;
     exit(1);
 }
 
-require_once $composerAutoload;
+require_once __DIR__ . '/vendor/autoload.php';
 
 if(!extension_loaded('protobuf')) {
-    Logger::error('protobuf module is not installed.');
+	Allegro\Protobuf\Compiler\Logger::error('protobuf module is not installed.');
     exit(1);
 }
 
