3179b5885026c80f21de0e4a6764c83c10c42889
Jack Noordhuis <daswaggamer@gmail.com>
2018-05-15 12:48:40 +1000

    Add executable wrapper and use composer autoloader

--- protoc-gen-php.php
+++ protoc-gen-php.php
@@ -1,34 +1,20 @@
 #!/usr/bin/env php
 <?php
-spl_autoload_register(function ($class)
-    {
-        $filename = implode(DIRECTORY_SEPARATOR, array(
-            __DIR__, 'src', str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php'
-        ));
-        if (file_exists($filename)) {
-            include($filename);
-            return true;
-        }
-        return false;
-    }
-);
 
 use Allegro\Protobuf\Compiler\Logger;
 
 $composerAutoload = __DIR__ . '/vendor/autoload.php';
-if (!file_exists($composerAutoload)) {
-    Logger::error('The dependencies are not installed. Run "composer install" in order to install them.');
-    exit(1);
-}
-
-if (!extension_loaded('protobuf')) {
-    Logger::error('protobuf module is not installed.');
+if(!file_exists($composerAutoload)) {
+    echo '[ERROR] The dependencies are not installed. Run "composer install" in order to install them.' . PHP_EOL . PHP_EOL;
     exit(1);
 }
 
 require_once $composerAutoload;
 
-use Allegro\Protobuf\Compiler\Compiler;
+if(!extension_loaded('protobuf')) {
+    Logger::error('protobuf module is not installed.');
+    exit(1);
+}
 
-$compiler = new Compiler();
+$compiler = new Allegro\Protobuf\Compiler\Compiler();
 $compiler->run(__FILE__);
