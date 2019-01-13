1066b1a3375e7b4af38fe5697c7d71c0b6384b91
Jack Noordhuis <daswaggamer@gmail.com>
2019-01-14 02:44:59 +1100

    Define phpext_protobuf_ptr

--- php_protobuf.h
+++ php_protobuf.h
@@ -9,5 +9,6 @@
 #define PHP_PROTOBUF_EXTNAME "protobuf"
 
 extern zend_module_entry protobuf_module_entry;
+#define phpext_protobuf_ptr &protobuf_module_entry
 
 #endif /* PROTOBUF_PHP_PHP_PROTOBUF_H */
